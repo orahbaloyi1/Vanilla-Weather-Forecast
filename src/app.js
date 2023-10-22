9a51fedaa1c8c515f31cc536f06f30ceba36996b
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-10-22 21:02:48 +0200

    Created a function

--- src/app.js
+++ src/app.js
@@ -55,6 +55,8 @@ function displayTemp(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -75,7 +77,7 @@ function displayFahrenheitTemperature(event) {
   fahrenheit.innerHTML = celciusTemperature * 2;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHtml = `<div class="row">`;
@@ -100,6 +102,15 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHtml;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 let celciusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -112,4 +123,3 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayCelciusTemperature);
 
 search("Mahikeng");
-displayForecast();
